172ae15c530422bfc1decb185a9fb5dffb4db89f
Sean Menezes <menezessean74@gmail.com>
2023-02-12 11:50:08 -0800

    Addd reverse string example

--- hello.go
+++ hello.go
@@ -1,8 +1,10 @@
 package main
 
-import "fmt"
-
+import(
+	"fmt"
+	"github.com/seanmenezes/go/hello/morestrings"
+)
 func main(){
-	fmt.Println("Hello, world.")
+	fmt.Println(morestrings.ReverseRunes("!oG, olleH"))
 }
 
